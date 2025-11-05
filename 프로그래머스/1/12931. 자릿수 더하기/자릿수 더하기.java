c1eba53a7153453127333e753e4272c2b83756ba
ju2hong <152269299+ju2hong@users.noreply.github.com>
2025-11-05 17:25:31 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 86.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        String str = Integer.toString(n);
+        int sum = 0;
+        for(int i=0; i<str.length(); i++) {
+            sum += str.charAt(i) - '0';
+        }            
+        return sum;
+    }
+}
