165f40cfa1779f25d43ee3897f10c4005838900e
ju2hong <152269299+ju2hong@users.noreply.github.com>
2025-11-28 17:05:02 +0900

    [level 0] Title: 세균 증식, Time: 0.02 ms, Memory: 91 MB -BaekjoonHub

---
+++ 프로그래머스/0/120910. 세균 증식/세균 증식.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int n, int t) {
+        int answer = 0;
+        for(int i=0; i<t; i++) {
+            n += n;
+        } 
+        answer = n;
+        return answer;
+    }
+}
